78ed81ccbe631875d17723f12357fe46ce3e6418
Zardoz <elaphe@outlook.it>
2019-12-04 11:21:43 +0100

    create form

---
+++ forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from import_export.resources import Resource
+
+
+class CsvCleanForm(forms.Form):
+    resource = Resource
+    field = forms.ChoiceField(label="")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        recipient_headers = [[field, field] for field in self.resource.fields]
+        CHOICE = [('-----', '------')] + recipient_headers
+        self.fields['field'].choices = CHOICE
