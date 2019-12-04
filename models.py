5c9b853a7a2a3a3d9508266a92ee268657b8daf2
Zardoz <elaphe@outlook.it>
2019-12-04 10:51:47 +0100

    create models

---
+++ models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.core.validators import FileExtensionValidator
+
+# Create your models here
+
+class CsvFile(models.Model):
+    file = models.FileField(
+        upload_to='media/',
+        validators=[FileExtensionValidator(allowed_extensions=['csv'])])
